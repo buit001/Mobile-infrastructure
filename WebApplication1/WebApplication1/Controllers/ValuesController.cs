a3bcb5f2374ee543eb4365548d32f0dca557369e
Siem van der Kluft <svdkluft@gmail.com>
2016-06-01 23:31:39 +0200

    werkt niet

---
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // Get api/values
+        public List<stappen> Get()
+        {
+            try
+            {
+                using (var db = new Model1())
+                {
+                    var steps = db.stappen.ToList();
+                    return steps;
+                }
+            }
+            catch(Exception e)
+            {
+                return new List<stappen>();
+            }
+        }
+
+        // Post api/values/5
+        public List<stappen> Post()
+        {
+            using (var db = new Model1())
+            {
+                db.stappen.Add();
+            }
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/stappen.cs
@@ -0,0 +1,28 @@
+namespace WebApplication1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("stappentellerdatabase.stappen")]
+    public partial class stappen
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int idStappen { get; set; }
+
+        [StringLength(45)]
+        public string aantalStappen { get; set; }
+
+        [StringLength(45)]
+        public string dag { get; set; }
+    }
+
+    public class stappenModel
+    {
+        public string aantalStappen { get; set; }
+        public string dag { get; set; }
+    }
+}
