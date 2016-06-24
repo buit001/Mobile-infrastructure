663554a60451e1632e9ee0c0ed119ee4166f6947
Tan <Tandemandiealleskan@hotmail.com>
2016-06-24 02:36:03 +0200

    postmethod

--- Stappenteller/app/src/main/java/com/example/siem/stappenteller/MainActivity.java
+++ Stappenteller/app/src/main/java/com/example/siem/stappenteller/MainActivity.java
@@ -7,16 +7,27 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -24,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView stepCounter;
     private Button stepList;
     public boolean step;
+    Map<Object, Object> params;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,4 +106,44 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             stepCounter.setText(String.valueOf(event.values[0]));
         }
     }
+
+    public Map<Object, Object> getParams() {
+        return params;
+    }
+
+    protected void onLoginEvent(String stappen, String dag) {
+
+
+            String url = getString(R.string.API_PostStappen);
+
+            params = new HashMap<Object, Object>();
+            params.put("stappen", stappen);
+            params.put("dag", dag);
+
+            final JSONObject jsonObject = new JSONObject(params);
+
+            JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                    (Request.Method.POST, url, jsonObject, new Response.Listener<JSONObject>() {
+
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            //  User type is Parent
+                            if (response.toString().contains("true")) {
+                                Toast.makeText(getApplicationContext(), "success", Toast.LENGTH_SHORT).show();
+                            }
+
+                            }
+
+            }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), "Could not connect, try again ", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+            // Access the RequestQueue through your singleton class.
+            NetworkManager.getInstance(this).addToRequestQueue(jsObjRequest);
+    }
+
 }
